6320f8d6cfcd508b22db22fa23a9375283a25f76
Michael <vmichaelkirkaldy@live.com>
2018-07-12 11:08:54 -0500

    updates to all files

--- apps/restful/urls.py
+++ apps/restful/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
   url(r'^users/create$', views.create),
   url(r'^users/(?P<id>\d+)/show$', views.show),
   url(r'^users/(?P<id>\d+)/edit$', views.edit),
-  url(r'^users/(?P<id>\d+)/update$', views.update),
-  url(r'^users/destroy$', views.destroy)
+  url(r'^users/update$', views.update),
+  url(r'^users/(?P<id>\d+)/destroy$', views.destroy)
 ]
